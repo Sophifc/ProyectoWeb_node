ef61cc73b9acce0d0b4de8747d30b48747ffc876
SpeedySW <80695724+speedysw@users.noreply.github.com>
2023-06-09 13:06:38 -0400

    Update navBar.js

--- src/views/navBar.js
+++ src/views/navBar.js
@@ -1,66 +1,48 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from '../images/logo.png';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from '../images/logo.png'
+import { Link } from 'react-router-dom';
+import React from 'react';
 
-import {Link} from 'react-router-dom';
 
-
-
-function NavBar() {
+function  NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg header">
-        <div className="container-fluid">
-              
-            <img src={logo}class="logo" ></img><a className="navbar-brand" >EcoSfera</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
+    <Navbar className="header">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={logo} className="logo"></img>EcoSfera</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav Nav className="justify-content-center align-items-center custom-nav">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/noticias">Noticias</Nav.Link>
+            <NavDropdown title="Animales" id="basic-nav-dropdown">
+              <NavDropdown.Item as={Link} to="/animal-por-dia">Animal por dia</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item as={Link} to="/extincion">Animal en peligro de extinción</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item as={Link} to="/animales">Animal por ecosistema</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Ecosistemas" id="basic-nav-dropdown">
+              <NavDropdown.Item as={Link} to="/ecosistema">Desertico</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.2">Marino</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.3">Terrestre</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link as={Link} to="/como-actuar" className="custom-nav-link">¿Como actuar?</Nav.Link>
+            <Nav.Link as={Link} to="/ayudanos">Ayudanos!</Nav.Link>
+          </Nav>
 
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                {/* <!-- <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li> --> */}
-                <li className="nav-item"><a class="nav-link" href="noticias.html">Noticias</a></li>
-                <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Ecosistemas</a>
-                  <ul className="dropdown-menu">
-                    <li><a className="dropdown-item" href="ecosistemas.html">Desértico</a></li>
-                    <li><a className="dropdown-item" href="ecosistemas.html">Marino</a></li>
-                    <li><a className="dropdown-item" href="ecosistemas.html">Terrestre</a></li>
-                  </ul>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Animales</a>
-                <ul className="dropdown-menu">
-                <li><a className="dropdown-item" href="animalpordia.html">Animal por dia</a></li>
-                <li><a className="dropdown-item" href="animales.html">Animales por ecosistema</a></li>
-                <li><a className="dropdown-item" href="extincion.html">Animales en peligro de extinción</a></li>
-                </ul>
-            </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="comoactuar.html">¿Cómo actuar?</a>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/Ecosistemas">Ayudanos</Link>
-                  {/* <a class="nav-link" href="ayudanos.html">Ayúdanos</a> */}
-                </li>
-              </ul>
-            </div>
-
-            <div>
-              <ul className="nav justify-content-end">
-                 <li className="nav-item">
-                      <a className="nav-link text-black" href="inicio.html">Ingresar</a>
-                 </li>
-                 <li className="nav-item">
-                     <a className="nav-link text-black" href="registro.html">Registrarse</a>
-                 </li>
-              </ul>
-            </div>
-
-        </div>
-    </nav>
+          <Nav>
+            <Nav.Link href="#home">Iniciar</Nav.Link>
+            <Nav.Link href="#link">Registrarse</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
