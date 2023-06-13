95de018e3d86eb478d5147c395098602b6a996c8
SpeedySW <80695724+speedysw@users.noreply.github.com>
2023-06-12 22:11:02 -0400

    Add files via upload

--- src/views/prueba.js
+++ src/views/prueba.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-
+const cors = require('cors');
 const app = express();
-const PORT = 3000;
+const PORT = 9000;
 
-// Middleware para analizar los cuerpos de las solicitudes
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
-// Ruta para guardar los datos del formulario en un archivo JSON
 app.post('/api/formulario', (req, res) => {
   const formData = req.body;
   
-  // Lee el archivo JSON existente (si existe)
+  
   let existingData = [];
   try {
     const data = fs.readFileSync('registro.json');
@@ -22,16 +21,15 @@ app.post('/api/formulario', (req, res) => {
     console.log('No se encontró ningún archivo JSON existente');
   }
 
-  // Agrega los nuevos datos al arreglo existente
+  
   existingData.push(formData);
 
-  // Guarda los datos en el archivo JSON
+  
   fs.writeFileSync('registro.json', JSON.stringify(existingData));
 
   res.json({ message: 'Los datos del formulario se han guardado correctamente' });
 });
 
-// Inicia el servidor
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
