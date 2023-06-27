40d8e8841aa818f7307b4bac6e97cd8288000d56
Sophifc <88857873+Sophifc@users.noreply.github.com>
2023-06-27 01:40:52 -0400

    Update navBar.jsx

--- src/views/navBar.jsx
+++ src/views/navBar.jsx
@@ -2,20 +2,21 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import logo from '../../images/logo.png'
+import logo from '../images/logo.png'
 import { Link } from 'react-router-dom';
 import React from 'react';
-import '../CSS/navbar.css'
+import './CSS/navbar.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useNavigate } from 'react-router-dom';
-import { AuthContext } from '../AuthContext'
+import { AuthContext } from './AuthContext'
 import { useContext } from "react";
-
+import Decodificar from './validarRol';
 
 function NavBar() {
   const {isLoggedIn ,setIsLoggedIn } = useContext(AuthContext);
   const navigate = useNavigate();
+  const userRole = Decodificar();
 
   const handleLogout = () => {
     setIsLoggedIn(false);
@@ -40,29 +41,38 @@ function NavBar() {
               <NavDropdown.Item as={Link} to="/animales">Animal por ecosistema</NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Ecosistemas" id="basic-nav-dropdown">
-              <NavDropdown.Item as={Link} to="/ecosistema">Desertico</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/ecosistemaDesierto">Desertico</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.2">Marino</NavDropdown.Item>
+              <NavDropdown.Item href="/ecosistema">Marino</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.3">Terrestre</NavDropdown.Item>
+              <NavDropdown.Item href="/ecosistemaSelva">Selvatico</NavDropdown.Item>
             </NavDropdown>
             <Nav.Link as={Link} to="/como-actuar" className="custom-nav-link">¿Como actuar?</Nav.Link>
             <Nav.Link as={Link} to="/ayudanos">Ayudanos!</Nav.Link>
           </Nav>
 
+          
           <Nav>
-            {isLoggedIn ? (
+          {isLoggedIn && (
               <>
+                {userRole === 'admin' && ( /* logeados admin */
+                  <Nav.Link as={Link} to="/adminConfig">Admin</Nav.Link>
+                )} 
                 <Nav.Link as={Link} to="/perfil">Perfil</Nav.Link>
                 <Nav.Link onClick={handleLogout}>Cerrar</Nav.Link>
               </>
-            ) : (
+            )}
+
+            {/* No logeados */}
+            {!isLoggedIn && (
               <>
                 <Nav.Link as={Link} to="/iniciar">Iniciar</Nav.Link>
-                <Nav.Link as={Link} to="/Registrarse">Registrarse</Nav.Link>
+                <Nav.Link as={Link} to="/registrarse">Registrarse</Nav.Link>
               </>
             )}
+
           </Nav>
+          
         </Navbar.Collapse>
       </Container>
     </Navbar>
@@ -70,3 +80,4 @@ function NavBar() {
 }
 
 export default NavBar;
+
