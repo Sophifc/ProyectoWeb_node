24c11254ce4debfb27df3684c334e0bb30915d2e
Sophifc <88857873+Sophifc@users.noreply.github.com>
2023-06-26 22:38:24 -0400

    Update noticias.js

--- src/views/noticias.js
+++ src/views/noticias.js
@@ -4,18 +4,20 @@ import { Fragment } from "react";
 
 import noticia1 from '../images/noticia1.jpg';
 import noticia2 from '../images/noticia2.jpg';
+import logo from '../images/logo.png';
 
 function Noticias() {
 
     return (
 
+        <div class="bodyNoticias">
         <><NavBar></NavBar>
 
             <Fragment>
                 <div class="row rowNoticias">
 
 
-                    <div class="card cardNoticias mb-3">
+                    <div class="card mb-3">
                         <div class="row g-0">
                             <div class="col-md-4">
                                 <img src={noticia1} class="img-fluid rounded-start" alt="Foto de una noticia"/>
@@ -31,7 +33,7 @@ function Noticias() {
                     </div>
 
 
-                    <div class="card cardNoticias mb-3" >
+                    <div class="card mb-3" >
                         <div class="row g-0">
                             <div class="col-md-4">
                                 <img src={noticia2} class="img-fluid rounded-start" alt="Foto de una noticia"/>
@@ -47,13 +49,42 @@ function Noticias() {
                     </div>
                 </div>
 
+                <div className="container-fluid py-3 footer">
+                    <div className="row">
+                        <div className="col-xs-12 col-md-6 col-lg-3">
+                            <li className="foooter">
+                                <img src={logo} class="logo-footer"/> 
+                                <h3>EcoSfera</h3> 
+                            </li>
+                        </div>
 
+                        <div className="col-xs-12 col-md-6 col-lg-3">
+                            <h5>¿Quienes somos?</h5>
+                            <p>Insertar descripcion</p>
+                        </div>
+
+                        <div className="col-xs-12 col-md-6 col-lg-3">
+                            <h5>Contactos</h5>
+                            <p>Dirección</p>
+                            <p>Correo Electronico</p>
+                            <p>Telefono</p>
+                        </div>
+
+                        <div className="col-xs-12 col-md-6 col-lg-3">
+                            <h5>Avisos</h5>
+                            <p>Terminos y Condiciones</p>
+                            <p>Politicas de Privacidad</p>
+                        </div>
+                    </div>
+                </div>
             </Fragment>
 
         </>
-
+        </div>
 
     );
 }
 
 export default Noticias;
+
+
