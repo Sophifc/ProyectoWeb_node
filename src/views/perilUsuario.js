70a08b0b0db1f8f673c827d7809737dd621d5ee3
Sophifc <88857873+Sophifc@users.noreply.github.com>
2023-06-26 23:06:36 -0400

    Add files via upload

---
+++ src/views/perilUsuario.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBTypography, MDBIcon } from 'mdb-react-ui-kit';
+import './CSS/perfilUsuario.css'
+
+function PerfilUsuario() {
+  return (
+    <section className="vh-100 bodyPerfil" >
+      <MDBContainer className="py-5 h-100">
+        <MDBRow className="justify-content-center align-items-center h-100">
+          <MDBCol  className="mb-4 mb-lg-0">
+            <MDBCard className="mb-3" style={{ borderRadius: '.5rem' }}>
+              <MDBRow className="g-0">
+                <MDBCol md="4" className="gradient-custom text-center text-white"
+                  style={{ borderTopLeftRadius: '.5rem', borderBottomLeftRadius: '.5rem' }}>
+                  <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
+                    alt="Avatar" className="my-5" style={{ width: '80px' }} fluid />
+                  <MDBTypography tag="h5">Marie Horwitz</MDBTypography>
+                  <MDBCardText>Web Designer</MDBCardText>
+                  <MDBIcon far icon="edit mb-5" />
+                </MDBCol>
+                <MDBCol md="8">
+                  <MDBCardBody className="p-4">
+                    <MDBTypography tag="h6">Information</MDBTypography>
+                    <hr className="mt-0 mb-4" />
+                    <MDBRow className="pt-1">
+                      <MDBCol size="6" className="mb-3">
+                        <MDBTypography tag="h6">Nombre</MDBTypography>
+                        <MDBCardText className="text-muted">info@example.com</MDBCardText>
+                      </MDBCol>
+
+
+                      <MDBCol size="6" className="mb-3">
+                        <MDBTypography tag="h6">email</MDBTypography>
+                        <MDBCardText className="text-muted">123 456 789</MDBCardText>
+                      </MDBCol>
+
+
+                      <MDBCol size="6" className="mb-3">
+                        <MDBTypography tag="h6">Phone</MDBTypography>
+                        <MDBCardText className="text-muted">123 456 789</MDBCardText>
+                      </MDBCol>
+
+                      <MDBCol size="6" className="mb-3">
+                        <MDBTypography tag="h6">Direccion</MDBTypography>
+                        <MDBCardText className="text-muted">123 456 789</MDBCardText>
+                      </MDBCol>
+
+                      <MDBCol size="6" className="mb-3">
+                        <MDBTypography tag="h6">Pais</MDBTypography>
+                        <MDBCardText className="text-muted">123 456 789</MDBCardText>
+                      </MDBCol>
+
+                      <MDBCol size="6" className="mb-3">
+                        <MDBTypography tag="h6">genero</MDBTypography>
+                        <MDBCardText className="text-muted">123 456 789</MDBCardText>
+                      </MDBCol>
+
+                    </MDBRow>
+
+
+                    <div className="d-flex justify-content-start">
+                      <a href="#!"><MDBIcon fab icon="facebook me-3" size="lg" /></a>
+                      <a href="#!"><MDBIcon fab icon="twitter me-3" size="lg" /></a>
+                      <a href="#!"><MDBIcon fab icon="instagram me-3" size="lg" /></a>
+                    </div>
+                  </MDBCardBody>
+                </MDBCol>
+              </MDBRow>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </section>
+  );
+}
+
+export default PerfilUsuario;
