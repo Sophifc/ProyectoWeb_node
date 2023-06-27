38b0f9189b38d36cb069e9ad164926fb590c7282
Sophifc <88857873+Sophifc@users.noreply.github.com>
2023-06-27 10:29:44 -0400

    Update noticias.js

--- src/views/noticias.js
+++ src/views/noticias.js
@@ -2,6 +2,7 @@ import './CSS/noticias.css';
 import NavBar from "./navBar";
 import { Fragment } from "react";
 
+
 import noticia1 from '../images/noticia1.jpg';
 import noticia2 from '../images/noticia2.jpg';
 import logo from '../images/logo.png';
@@ -23,10 +24,10 @@ function Noticias() {
                                 <img src={noticia1} class="img-fluid rounded-start" alt="Foto de una noticia"/>
                             </div>
                             <div class="col-md-8">
-                                <div class="card-body">
-                                    <h5 class="card-title">Noticia desierto</h5>
-                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                            <div class="card-body">
+                                    <h5 class="card-title">Nido de vinchucas en el desierto</h5>
+                                    <p class="card-text">Cerca de la zona del conocido Desierto de Atacama son encontrados varios nidos del peligroso animal llamado Vinchuca, una de las especies mas peligrosas del mundo</p>
+                                    <p class="card-text"><small class="text-body-secondary">Last updated 3 days ago</small></p>
                                 </div>
                             </div>
                         </div>
@@ -40,9 +41,9 @@ function Noticias() {
                             </div>
                             <div class="col-md-8">
                                 <div class="card-body">
-                                    <h5 class="card-title">Noticia nieve</h5>
-                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                                    <h5 class="card-title">Problemas en ecosistemas nevados</h5>
+                                    <p class="card-text">Los efectos del cambio climatico estan afectando poco a poco a los ecosistemas mas frios, generando tanto problemas de supervivencia, como de habiat para ciertas especiess</p>
+                                    <p class="card-text"><small class="text-body-secondary">Last updated 1 day ago</small></p>
                                 </div>
                             </div>
                         </div>
